9dac5cc3bda9c543b8c44c6d263d846b831cc172
rahneverd <rahneverd@gmail.com>
2024-05-14 21:09:40 +0500

    today's session done

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import inquirer from 'inquirer';
 interface Student {
   name: string;
   id: number;
-  courses: string[];
+  courses: { name: string, id: number, tutionFee: number }[];
   balance: number;
 }
 
@@ -23,7 +23,7 @@ let courses: { name: string, id: number, tutionFee: number }[] = [{
   tutionFee: 2000
 }]
 
-let isRunning : boolean = true
+let isRunning: boolean = true
 while (isRunning) {
   const answers = await inquirer.prompt([
     {
@@ -33,23 +33,94 @@ while (isRunning) {
       choices: [
         'Add Student',
         'View Students',
+        'Manage Student',
         'Exit',
       ]
     }
   ])
-  
+
   switch (answers.action) {
     case 'Add Student':
-      console.log('Add Student case')
+      // add new student
+      const newUserInput = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Enter student name?'
+        },
+        {
+          type: 'number',
+          name: 'balance',
+          message: 'Enter student balance?'
+        }
+      ])
+      students.push({
+        name: newUserInput.name,
+        id: 10000 + students.length,
+        balance: newUserInput.balance,
+        courses: []
+      })
+      console.log(newUserInput.name + ' has been added.')
       break;
     case 'View Students':
-      console.log('View Student case')
+      // console.table(students)
+      // students.forEach((student) => {
+      //   console.log(student.name)
+      // })
+      for (let student of students) {
+        console.log(student.name)
+      }
+      break;
+
+    case 'Manage Student':
+      const choices = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What do you want to do?',
+          choices: [
+            'Enroll In A Course',
+            'Pay Fee',
+            'Update Balance',
+            'Exit',
+          ]
+        }
+      ])
+      switch (choices.action) {
+        case 'Enroll In A Course':
+          console.log('Available Courses: ')
+          console.table(courses)
+          console.log('Available Students: ')
+          console.table(students)
+          const courseInput = await inquirer.prompt([
+            {
+              type: 'number',
+              name: 'courseId',
+              message: 'Enter course id?'
+            },
+            {
+              type: 'number',
+              name:'studentId',
+              message: 'Enter student id?'
+            }
+          ])
+          students[courseInput.studentId - 10000].courses.push(courses[courseInput.courseId])
+          console.log(students[courseInput.studentId - 10000].name +' has been enrolled in '+ courses[courseInput.courseId].name)
+          break;
+        case 'Pay Fee':
+          break;
+        case 'Update Balance':
+          break;
+        case 'Exit':
+          break;
+        default:
+          break;
+      }
       break;
     case 'Exit':
-      console.log('Exit case')
       isRunning = false
       break;
-  
+
     default:
       isRunning = false
       break;
